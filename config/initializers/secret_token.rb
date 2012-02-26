0616ea61586cb209ceea6854c8f9a43b2b72f4b9
Juan Ripoll <juarinu@gmail.com>
2012-02-26 20:34:31 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Publihot::Application.config.secret_token = 'e51575c22f1addb17a35a1d50f72b796837a88f5c0d99b15278b0448e10b612db7e804a2082a2f53e24299c4195fc819c86a0ddb6896d9465d959dfffc5a5555'
